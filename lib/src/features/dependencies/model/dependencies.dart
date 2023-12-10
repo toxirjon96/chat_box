b967d64afd9bca920639d5846991a8cedaa73302
toxirjon96 <tatu.toxirjon@gmail.com>
2023-12-10 21:22:08 +0500

    app modified

---
+++ lib/src/features/dependencies/model/dependencies.dart
@@ -0,0 +1,7 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+base class Dependencies {
+  Dependencies();
+
+  late final SharedPreferences sharedPreferences;
+}
---
+++ lib/src/features/dependencies/widget/dependencies_scope.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import '../model/dependencies.dart';
+
+class DependenciesScope extends InheritedWidget {
+  const DependenciesScope({
+    required super.child,
+    required this.dependencies,
+    super.key,
+  });
+
+  final Dependencies dependencies;
+
+  static DependenciesScope? maybeOf(BuildContext context,
+          {bool listen = false}) =>
+      listen
+          ? context.dependOnInheritedWidgetOfExactType<DependenciesScope>()
+          : (context
+              .getElementForInheritedWidgetOfExactType<DependenciesScope>()
+              ?.widget as DependenciesScope);
+
+  static Never _notFoundInheritedWidgetOfExactType() => throw ArgumentError(
+        'Out of scope, not found inherited widget '
+            'a DependenciesScope of the exact type',
+        'out_of_scope',
+      );
+
+  static DependenciesScope of(BuildContext context, {bool listen = false}) =>
+      maybeOf(context, listen: listen) ?? _notFoundInheritedWidgetOfExactType();
+
+  @override
+  bool updateShouldNotify(covariant InheritedWidget oldWidget) => false;
+}
